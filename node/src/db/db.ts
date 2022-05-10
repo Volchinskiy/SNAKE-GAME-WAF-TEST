24a45ff9c6083220435b19ffd8b1196ae28b59c6
Maxim Volchinskiy <thyxaxa@gmail.com>
2022-05-10 10:23:51 +0300

    test commit

--- node/src/db/db.ts
+++ node/src/db/db.ts
@@ -1,11 +1,11 @@
 import Pg from "pg";
 
 const pool = new Pg.Pool({
-  user: "postgres",
-  password: "qwerty",
-  host: "localhost",
+  host: "ec2-176-34-211-0.eu-west-1.compute.amazonaws.com",
+  database: "di2e9p35ftokq",
+  user: "xffuzqwfeveywu",
   port: 5432,
-  database: "waf_test"
+  password: "05a3a384e1dd48bffaca500a667329a01baf28b732be4d7aa8dfe8bcba9dd847",
 });
 
 export default pool;
